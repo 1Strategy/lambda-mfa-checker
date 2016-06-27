fe92f478e87b3a3840f89fff54d0695a72a3afbd
Jordan Farrer <jordan@1strategy.com>
2016-06-27 10:21:35 -0600

    Initial commit (from Chase Spencer)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lambda MFA Checker
+
+This project consists of an AWS Lambda function that will check whether each IAM user has an MFA device (virtual and/or physical) setup.
+
+For a more detailed walkthrough of this project, see the blog post.
---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+import boto3
+def lambda_handler(context,event):
+   
+    client                  = boto3.client('iam')
+    sns                     = boto3.client('sns')
+    response                = client.list_users()
+    userVirtualMfa          = client.list_virtual_mfa_devices()
+    mfaNotEnabled           = []
+    virtualEnabled          = []
+    physicalString          = ''
+    
+    # loop through virtual mfa to find users that actually have it
+    for virtual in userVirtualMfa['VirtualMFADevices']:
+        virtualEnabled.append(virtual['User']['UserName'])
+           
+    # loop through users to find physical MFA
+    for user in response['Users']:
+        userMfa  = client.list_mfa_devices(UserName=user['UserName'])
+        
+        if len(userMfa['MFADevices']) == 0:
+            if user['UserName'] not in virtualEnabled:
+                mfaNotEnabled.append(user['UserName']) 
+    
+    
+    if len(mfaNotEnabled) > 0:
+        physicalString = 'Physical & Virtual MFA is not enabled for the following users: \n\n' + '\n'.join(mfaNotEnabled)
+    else:
+        physicalString = 'All Users have Physical and Virtual MFA enabled'
+    
+    response = sns.publish(
+        TopicArn='arn:aws:sns:us-west-2:842337631775:MFA_Not_Enabled',
+        Message= physicalString,
+        Subject='Enable MFA',
+    )
+    
+    return mfaNotEnabled
+    
